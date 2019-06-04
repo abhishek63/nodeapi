b1e549ae8b8b237969e62b8e35e5a088aade7d88
Abhishek Kumar <kumargolu63@gmail.com>
2019-06-04 22:22:43 +0530

    use morgan as middleware

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-const PORT = 8080;
+//middleware used
+app.use(morgan('dev'))
 
+
+//routes
+app.get('/',function(req,res){
+    res.send("Hello world")
+})
+
+const PORT = 8080;
 
 app.listen(PORT,function(){
     console.log(`server is running on port ${PORT}` );
