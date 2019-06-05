8ae14fddeae365c4eb00c8e5340342ce90029d44
Abhishek Kumar <kumargolu63@gmail.com>
2019-06-05 21:15:54 +0530

    write step and flow of signin & install jsonwebtoken and cookie-parser

--- routes/route.js
+++ routes/route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const Post = require("../models/Post");
 const User = require("../models/User");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const expressValidator = require("express-validator");
 const validator = require("../validators");
 
 const router = express.Router();
 router.use(bodyParser());
+router.use(cookieParser())
 router.use(expressValidator());
 
 //getting all the result from database
@@ -43,4 +45,25 @@ router.post("/signup",validator.userSignupValidator, async function(req, res) {
     await user.save();
     res.status(200).json({ message: "Signup success! Please login." });
 });
+
+//signin
+router.post('/signin',function(req,res){
+
+    //find user based on email and password
+
+
+    //if error and no user
+
+
+    //if user exits then authenticate
+
+
+    //generate a token with userid and secret key which is in .env
+
+
+    //store the token as cookie with expiry time
+
+
+    //return response token to the cilent 
+})
 module.exports = router;
